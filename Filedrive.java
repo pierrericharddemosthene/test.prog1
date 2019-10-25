4ede74940eb29488004dca7e0bf64be2fb72386e
pierre <pierrericharddemosthene@yahoo.fr>
2019-10-25 11:57:35 -0300

    leia um arquivo

---
+++ Filedrive.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Filedrive{
+	public static void main(String[]args){
+		File file= new File("test.txt");
+		try{
+			FileReader fileReader=new FileReader(file);
+			BufferedReader bufferedReader=new BufferedReader(fileReader);
+			String linha= bufferedReader.readline();
+			System.out.println(linha);
+		}catch(FileNotFoundException e){
+			System.err.printf("o arquivo não encontrou");
+
+		}catch(I0Exception e){
+			System.out.println("é impossivel pra ler o conteudo do arquivo");
+		}
+	}
+}
